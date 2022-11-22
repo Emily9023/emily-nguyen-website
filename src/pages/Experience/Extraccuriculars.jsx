a652f0ce04cfd68aee57ed9f48769a7c4b199da4
Emily Nguyen <emily.linh.nguyen@gmail.com>
2022-11-21 21:04:27 -0500

    images for experience

--- src/pages/Experience/Extraccuriculars.jsx
+++ src/pages/Experience/Extraccuriculars.jsx
@@ -6,26 +6,6 @@ import { ExperienceDisplay } from '../../components/experiences/experienceDispla
 const Extraccuriculars = () => {
   return (
     <ExperienceDisplay positions={experienceContent.extracurriculars}></ExperienceDisplay>
-    // <div className="extracurriculars-container">
-    //   {experienceContent.extracurriculars.map((content, index) => (
-    //     <div className="extracurriculars-experience-container">
-    //       <img src={"temp"} alt={content.altImage}></img>
-    //       <div className='extrcurriculars-experience-content'>
-    //         <h2> {content.title} </h2>
-    //         {content.description}
-    //         {content.positions.map((position, index) => (
-    //           <div>
-    //             <h3> {position.title} </h3>
-    //             {position.description.map((point, index) => (
-    //               <li>{point}</li>
-    //             ))}
-    //           </div>
-    //         ))}
-
-    //       </div>
-    //     </div>
-    //   ))}
-    // </div>
   );
 };
 
