d85868e7febfa43ff1f3cd31087e6fc40a2a9668
Emily Nguyen <emilyy.nguyen@mail.utoronto.ca>
2023-08-07 14:36:01 -0700

    one page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './App.css';
+import { Navbar } from './components/Navbar/Navbar';
+
+import { pages } from './util/pages';
+
+
+function App() {
+  let component 
+  const path = window.location.pathname
+  switch(path) {
+    case "/":
+      pages.navbar.forEach(page => {
+        if (page.path === path) {
+          component = page.component
+        }
+      });
+      break;
+    case "/about":
+      pages.navbar.forEach(page => {
+        if (page.path === path) {
+          component = page.component
+        }
+      });
+    break;
+    case "/experience":
+      pages.navbar.forEach(page => {
+        if (page.path === path) {
+          component = page.component
+        }
+      });
+    break;
+    case "/portfolio":
+      pages.navbar.forEach(page => {
+        if (page.path === path) {
+          component = page.component
+        }
+      });
+    break;
+    default:
+      component = pages.page404.component
+      break;
+  }
+  return (
+    <>
+      <Navbar/>
+      <div className='App-container-main'>
+        {component}
+      </div>
+    </>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
